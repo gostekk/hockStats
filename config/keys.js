f0dd277d6233c400e37bbc9caeb1a47499bd1d9b
gostekk <gostekk94@gmail.com>
2018-05-01 21:10:21 +0200

    Init #2

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoURI:
+    "mongodb://wolvesrenew:wolvesrenew1234@ds261969.mlab.com:61969/wolvesrenew",
+  port: 5000
+};
---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const config = require("./config/keys");
+
+const app = express();
+
+// Body parser
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// DB config
+const db = config.mongoURI;
+
+// Connect to MongoDB
+mongoose
+  .connect(db)
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.log(err));
+
+app.listen(config.port, () =>
+  console.log(`Server running on port ${config.port}`)
+);
+
+module.exports = app;
